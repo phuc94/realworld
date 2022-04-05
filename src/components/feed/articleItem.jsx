65c9800e44888d753045694db6143f8944c1494e
phuc <trungphuc94@gmail.com>
2022-04-05 14:28:22 +0700

    refactor articleItem

--- src/components/feed/articleItem.jsx
+++ src/components/feed/articleItem.jsx
@@ -1,33 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { favoriteArticleAPI, unfavoriteArticleAPI } from "../../service/article";
 import Author from "../author/author";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ArticleItem = ({ data }) => {
-  const [isFavorited, setIsFavorited] = useState(data.favorited);
+  const [itemData, setItemData] = useState({});
+
+  useEffect(() => {
+    if (data == undefined) return;
+    setItemData(data);
+  }, [data])
+
   const [isPending, setIsPending] = useState(false);
   const isAuthorized = useSelector(state => state.user.authorized);
   const navigate = useNavigate();
 
   const toArticleDetail = () => {
-    navigate(`/article/${data.slug}`)
+    navigate(`/article/${itemData.slug}`)
   };
 
   const onFavorite = e => {
     if (isPending) return;
     setIsPending(prev => !prev);
-    if (isAuthorized && isFavorited) {
-      unfavoriteArticleAPI(data.slug).then(res => {
+    if (isAuthorized && itemData.favorited) {
+      unfavoriteArticleAPI(itemData.slug).then(res => {
         if (res.status == 200) {
-          setIsFavorited(prev => !prev);
+          setItemData(res.data.article);
           setIsPending(prev => !prev);
         }
       })
     } else {
-      favoriteArticleAPI(data.slug).then(res => {
+      favoriteArticleAPI(itemData.slug).then(res => {
         if (res.status == 200) {
-          setIsFavorited(prev => !prev);
+          setItemData(res.data.article);
           setIsPending(prev => !prev);
         }
       })
@@ -37,26 +43,26 @@ const ArticleItem = ({ data }) => {
   return (
     <div className="feed-tab">
       <div className="feed-tab__info">
-        <Author author={data.author} createdAt={data.createdAt} />
+        <Author author={itemData.author} createdAt={itemData.createdAt} />
         <button
           onClick={e => onFavorite(e)}
           className={`feed-tab__favorite
-            ${isFavorited ? '--favorited' : ''}
+            ${itemData.favorited ? '--favorited' : ''}
             ${isPending ? '--pending' : ''}
             `}
         >
           <i className="ion-heart"></i>
-          <span> {data.favoritesCount} </span>
+          <span> {itemData.favoritesCount} </span>
         </button>
       </div>
       <div onClick={() => toArticleDetail()} className="feed-tab__content">
-        <h1>{data.title}</h1>
-        <p>{data.description}</p>
+        <h1>{itemData.title}</h1>
+        <p>{itemData.description}</p>
       </div>
       <div className="feed-tab__cta">
         <span onClick={() => toArticleDetail()} className="feed-tab__read-more">Read more...</span>
         <div className="feed-tab__tags">
-          {data.tagList.map(tag => (
+          {itemData?.tagList?.map(tag => (
             <span onClick={() => toArticleDetail()}>{tag}</span>
           ))
           }
